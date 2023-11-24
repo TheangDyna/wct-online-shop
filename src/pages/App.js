c22be05040347215f172123e578702e7dee80a35
11RothMony <Rothmony8102003@gmail.com>
2023-11-24 15:01:36 +0700

    Navbar

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import TestButton from "../components/TestButton";
 import CategoryCard from "../components/CategoryCard";
+import Navbar from "../components/Navbar";
 
 const App = () => {
   return (
     <div className="">
       <h1>Home Page</h1>
       <TestButton></TestButton>
+      <Navbar></Navbar>
       <CategoryCard></CategoryCard>
     </div>
   );
